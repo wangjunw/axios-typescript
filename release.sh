0eb59a05a8f85e4476c06e1a38bbe914c2808ee5
junwei <1157593964@qq.com>
2019-11-22 21:27:08 +0800

    '脚本报错调试'

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@
 set -e
 echo "Enter release version："
 read VERSION
-read -p "Releasing $VERSION - are you sure?(y/n)" -n 1 -r
+read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
 echo # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
